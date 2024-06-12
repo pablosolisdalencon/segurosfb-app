96ebc9da4f5fcee7a079f2edd41d41f7f6949db2
pablosolisdalencon <pablo.solis.dalencon@gmail.com>
2024-06-12 00:08:01 -0400

    refresh base. v1

--- src/app/web/mkt/automotrices/automotrices.component.ts
+++ src/app/web/mkt/automotrices/automotrices.component.ts
@@ -1,5 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { MatDialogRef} from '@angular/material/dialog';
+import { MAT_DIALOG_DATA} from '@angular/material/dialog' 
 
+export interface DialogData {
+  animal: string;
+  name: string;
+}
+
+/**
+ * @title Dialog Overview
+ */
 @Component({
   selector: 'app-automotrices',
   standalone: true,
@@ -8,5 +19,38 @@ import { Component } from '@angular/core';
   styleUrl: './automotrices.component.css'
 })
 export class AutomotricesComponent {
+
+  animal!: string;
+  name!: string;
+
+  constructor(public dialog: MatDialog) {}
+
+  openDialog(): void {
+    const dialogRef = this.dialog.open(DialogOverviewExampleDialog, {
+      width: '80%',
+      data: {name: this.name, animal: this.animal}
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      console.log('The dialog was closed');
+      this.animal = result;
+    });
+  }
+
+}
+
+@Component({
+  selector: 'seguros-automoviles',
+  templateUrl: './seguros-automoviles.html',
+})
+export class DialogOverviewExampleDialog {
+
+  constructor(
+    public dialogRef: MatDialogRef<DialogOverviewExampleDialog>,
+    @Inject(MAT_DIALOG_DATA) public data: DialogData) {}
+
+  onNoClick(): void {
+    this.dialogRef.close();
+  }
 
 }
